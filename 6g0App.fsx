85e226138a41f86f5db4e8260cb58736508f2a18
Róar Mørkøre Hansen <112826687+Roar-Morkore-Hansen@users.noreply.github.com>
2022-10-31 18:26:30 +0100

    bla bla

--- 6g0App.fsx
+++ 6g0App.fsx
@@ -8,7 +8,13 @@ open System
 let height:int = 3
 let width:int = 3
 
-let a:state = [(Red,(0,0));(Red,(1,0));(Red,(2,0));(Blue,(0,1));(Red,(1,1));(Red,(2,1));(Red,(0,2));(Red,(1,2));(Green,(2,2))]
-printfn "%A" (addRandom Blue (shiftLeft a))
+let a:state = [(Green,(0,0));(Green,(1,0));(Red,(2,0));(Blue,(0,1));(Red,(1,1));(Red,(2,1));(Red,(0,2));(Red,(1,2));(Green,(2,2))]
+let test: state = [(Red, (0,0));(Blue, (1,0));(Red, (2,0));(Red, (0,1)); (Blue, (1,1)); (Blue, (2,1))]
+let test1: state = [(Red, (0,0));(Blue, (1,0));(Red, (0,1));(Green, (1,1));(Green, (2,1))]
+
+let s:state = test1 
+
+
+
 
 do runApp "2048" 600 600 draw react [(Red,(0,0));(Blue,(1,0))]
--- 6g0Lib.fs
+++ 6g0Lib.fs
@@ -1,12 +1,13 @@
 module Solitaire
-
+open Canvas
 type pos = int * int 
 type value = Red | Green | Blue | Yellow | Black
 type piece = value * pos 
 type state = piece list 
-let length:int = 3
+let height: int = 3
+let width: int = 3
 
-let fromValue (v:value):Canvas.color=
+let fromValue (v: value) : Canvas.color =
     match v with
         |Red -> Canvas.red
         |Green -> Canvas.green
@@ -14,7 +15,7 @@ let fromValue (v:value):Canvas.color=
         |Yellow -> Canvas.yellow
         |Black -> Canvas.black
 
-let nextColor (c:value):value =
+let nextColor (c: value) : value =
     match c with
         |Red -> Green
         |Green -> Blue
@@ -22,36 +23,79 @@ let nextColor (c:value):value =
         |Yellow -> Black
         |Black -> Black
 
-let filter (k:int) (s:state):state=
-    s |> List.filter (fun (_, (_, y)) -> if y = k then true else false)
-
-let shiftLeft (s:state):state=
-    let shift (k:int) (s:state): state=
-        let mutable rFin:state= []
-        let rec merge (r:state)=
-            if r.IsEmpty = false then
-                let (headColor, (x,y)) = r.Head
-                let (tailColor, _) = r.Tail.Head
-                if r.Tail.IsEmpty = false && headColor = tailColor then
-                    rFin <- rFin :: [nextColor(headColor),(x,y)]
-                    merge (r.Tail.Tail)
-                else
-                    rFin <- rFin :: [r.Head]
-                    merge (r.Tail)
-        merge (filter k ( List.sortBy (fun (_, (x, _)) -> x) s))
-        List.mapi (fun index (v,(x,y)) -> (v,(index, y))) rFin
-    (shift 0 s) :: (shift 1 s) :: (shift 2 s)
-    
-let flipUD (s:state):state=
-    s |> List.map (fun (value, (x,y)) -> (value ,(2 - x, y)))
-    
-let transpose (s:state):state=
-    s |> List.map (fun (value, (x,y)) -> (value, (y,x)))
+let filter (k: int) (s: state) : state=
+    s |> List.filter (fun (_, (_, y)) -> y = k )
 
 let empty (s: state) : pos list =
-    let height:int = 3
-    let width:int = 3
-    let spos = (s |> List.map (fun (_,(x,y)) -> (x,y)))
+    let sPos = (s |> List.map (fun (_,pos) -> pos))
     let lst = List.init (height * width) (fun i -> (i % width, i/height))
-    lst |> List.except spos
+    lst |> List.except sPos
 
+let rec shiftLeft (s: state) : state =
+    let mergedRow: state = List.empty
+    let rec merge (s: state) : state = 
+        // try defining elm and rst using try function
+        match s with 
+            //compare Value and y-axis of head and next element in list
+            | elm::rst when (List.isEmpty rst = false && fst(elm) = fst(rst.Head) && snd(snd(elm)) = snd(snd(rst.Head))) ->
+                (nextColor(fst(elm)), snd(elm)) :: (merge rst.Tail)
+            | elm::rst ->
+                elm :: (merge rst)
+            | [] ->
+                mergedRow
+
+    let mutable shiftedRow: state = List.empty
+    let rec shift (s: state) : state = 
+        match s with
+            | elm::rst ->
+                let (value, (x,y)) = elm
+                let emptyLst = (empty shiftedRow)
+                let shiftPos = (emptyLst |> (List.filter (fun (_,empy) -> empy = y))).Head
+                
+                shiftedRow <- (shiftedRow @ [(value, shiftPos)])
+                (shift rst)
+            | [] ->
+                shiftedRow
+    shift (merge (s |> List.sortBy (fun (_, (x, y)) -> y, x)))
+
+
+let flipLR (s: state) : state =
+    s |> List.map (fun (v, (x,y)) -> (v, ((width - 1 - x),y)))
+    
+let transpose (s: state) : state =
+    s |> List.map (fun (v, (x,y)) -> (v, (y,x)))
+
+let addRandom (c:value) (s:state) :state option =
+    let emptyPositions:pos list = empty s
+    if emptyPositions.IsEmpty = true then
+        None
+    else 
+        let rnd = System.Random()
+        let i = rnd.Next(0,(emptyPositions.Length))
+        Some (s @ [(c,(List.item i emptyPositions))])
+
+
+let draw (w: int) (h: int) (s: state): canvas =
+    let C: canvas = Canvas.create w h
+    s |> List.iter (fun (v,(x,y)) -> Canvas.setFillBox  C (fromValue v) ((w/width*x),(h/height*y)) ((w/width*(x+1)),(h/height*(y+1))))
+    C
+
+let react (s: state)(k: key) : state option =
+    printfn "s ======= %A" s
+    let mutable sNew: state = []
+    match getKey k with
+        | LeftArrow -> sNew <- (shiftLeft s)
+        | RightArrow -> sNew <-(flipLR (shiftLeft (flipLR s)))
+        | UpArrow -> sNew <-(transpose (shiftLeft (transpose s)))
+        | DownArrow -> sNew <-(transpose (flipLR(shiftLeft (flipLR(transpose s)))))
+        | _ -> ()
+    match sNew with
+        | elm::rst when (empty sNew).IsEmpty = false && (List.sort s = List.sort sNew) = false ->
+            printfn "%A - %A" s sNew
+            sNew <- Option.get(addRandom Red sNew)
+            printfn "sNew --- %A" sNew
+            Some(sNew)
+        | _ -> 
+            Some(s)
+
+        
